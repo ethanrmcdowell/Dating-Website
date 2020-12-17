9a76a148a1d0921fa51d5a5a26442e210ecb4398
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-12-16 19:34:26 -0500

    Removed unnecessary code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
 app.use(express.static("public"));
 
@@ -10,8 +11,6 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// let routes = require("./controllers/burgers_controller.js");
 
 // app.use("/", routes);
 
