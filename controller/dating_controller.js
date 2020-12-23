3d1cded88983e7e3c9a7ebd24f822b096e1b3a5a
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-12-23 12:17:09 -0500

    Profile page renders user data and Minion connections

--- controller/dating_controller.js
+++ controller/dating_controller.js
@@ -22,7 +22,54 @@ router.get("/", (req, res) => {
 });
 
 router.get("/profile", (req, res) => {
-    res.render("profile");
+    // Query for finding other users that match hobbies. 
+    // Need to get this info from page. At the moment, the current user's data is hardcoded
+    let currentUser = "username1";
+    let favoriteHobbies = ['Karate', 'Music', 'DIY'];
+
+    db.User.findAll({
+        attributes: ['username', 'hobby1id', 'hobby2id', 'hobby3id'],
+        where: {
+          [Op.or]: [
+            {hobby1id: favoriteHobbies},
+            {hobby2id: favoriteHobbies},
+            {hobby3id: favoriteHobbies}
+          ],
+          username: {
+            [Op.ne]: currentUser
+          }
+        }
+      })
+      .then(usersData => {
+        let userArray = [];
+        usersData.forEach(user => {
+          userArray.push(user.dataValues)
+        });
+
+        // This Query gets the user's info for their profile.
+        // Once again, the Current User is hardcoded for now.
+        db.User.findOne({
+            where: {
+                username: currentUser
+            }
+        }).then(response => {
+            let profileInfo = {
+                username: response.dataValues.username,
+                personalStatement: response.dataValues.personalStatement,
+                hobby1id: response.dataValues.hobby1id,
+                hobby2id: response.dataValues.hobby2id,
+                hobby3id: response.dataValues.hobby3id,
+                avatarURL: response.dataValues.avatarURL
+            }
+            console.log(profileInfo);
+
+            // This bundles up the results of both queries into an object to pass to handlebars
+            let hbsObjecct = {minionConnections: userArray, userProfile: profileInfo};
+            
+            console.log(hbsObjecct);
+            res.render("profile", hbsObjecct);
+        })
+      });
 });
 
 router.get("/login", (req, res) => {
@@ -37,33 +84,6 @@ router.post("/signup", (req, res) => {
     res.render("signup");
 });
 
-
-    // // // Query for finding other users that match hobbies. 
-    // // Need to get this info from page. At the moment is hardcoded
-    // let currentUser = 1;
-    // let favoriteHobbies = ['Karate', 'Music', 'DIY'];
-    // db.User.findAll({
-    //     attributes: ['username', 'hobby1id', 'hobby2id', 'hobby3id'],
-    //     where: {
-    //       [Op.or]: [
-    //         {hobby1id: favoriteHobbies},
-    //         {hobby2id: favoriteHobbies},
-    //         {hobby3id: favoriteHobbies}
-    //       ],
-    //       username: {
-    //         [Op.ne]: currentUser
-    //       }
-    //     }
-    //   })
-    //   .then(usersData => {
-    //     let userArray = [];
-    //     usersData.forEach(user => {
-    //       userArray.push(user.dataValues)
-    //     });
-    //     let connectionsObject = {data: userArray};
-    //     console.log(connectionsObject);
-    //     // Render with Handlebars with an #each helper
-    //   });
 
 
 module.exports = router;
