47d524c7901caf7ccd23f5c6e032ce97af051d40
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2021-01-05 21:16:30 -0500

    users can send messages from profile

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -2,5 +2,23 @@ $(document).ready(function() {
     let currentUser = $("#username").text();
     $("#updateProfile").on("click", () => {
         window.location.replace("/settings/" + currentUser);
-    })
+    });
+
+    $("#viewMessagesButton").on("click", () => {
+        window.location.replace("/messages/" + currentUser);
+    });
+
+    $(".messageUserButton").on("click", (e) => {
+        let receiver_username = $(e.target).attr("value");
+        let message = prompt(`What would you like to say to ${receiver_username}?`);
+        sendMessage(receiver_username, message);
+    });
+
+    function sendMessage(receiver_username, message) {
+        $.post("/replyMessage", {
+            receiver_username: receiver_username,
+            message: message,
+            senderUsername: currentUser
+        });
+    }
 });
