adf980dfa23fecc217122f3c9f4da5a943a05c59
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2021-01-07 16:45:21 -0500

    Added phony users based on real friends

--- seed.js
+++ seed.js
@@ -3,19 +3,20 @@ const db = require('./models');
 const seed = {
     seedUsers: function(){
         db.User.bulkCreate([
-            {username: "username1", password: 1, hobby1id: 'DIY', hobby2id: 'Karate', hobby3id: 'Music', personalStatement: "A little about me: Live, Love, Banana!", avatarURL: "../assets/img/diy.jpg"},
-            {username: "username2", password: 1, hobby1id: 'Karate', hobby2id: 'Music', hobby3id: 'Travel'},
-            {username: "username3", password: 1, hobby1id: 'Music', hobby2id: 'Travel', hobby3id: 'Weight-Lifting'},
-            {username: "username4", password: 1, hobby1id: 'Travel', hobby2id: 'Weight-Lifting', hobby3id: 'Wrestling'},
+            {username: "Emily-GrinionMinion", password: 1, hobby1id: 'Karate', hobby2id: 'Music', hobby3id: 'Travel', avatarURL: '../assets/img/diy.jpg'},
+            {username: "Cryin-Brian", password: 1, hobby1id: 'Music', hobby2id: 'Travel', hobby3id: 'Weight-Lifting', avatarURL: "../assets/img/karate.jpg"},
+            {username: "Megan-MegaMinion", password: 1, hobby1id: 'Travel', hobby2id: 'Weight-Lifting', hobby3id: 'Wrestling', avatarURL: "../assets/img/music.jpg"},
+            {username: "hunkkid123", password: 1, hobby1id: 'DIY', hobby2id: 'Weight-Lifting', hobby3id: 'Wrestling', avatarURL: "../assets/img/weight-lifting.jpg"},
+            {username: "IZK-So-Fresh-Man", password: 1, hobby1id: 'DIY', hobby2id: 'Travel', hobby3id: 'Wrestling', avatarURL: "../assets/img/wrestling.jpg"},
         ])
     },
-    seedMessages: function(){
-        db.Message.bulkCreate([
-            {sender_username_id: 1, receiver_username: "username2", message: "message1"},
-            {sender_username_id: 1, receiver_username: "username2", message: "message2"},
-            {sender_username_id: 1, receiver_username: "username2", message: "message3"}
-        ])
-    }
+    // seedMessages: function(){
+    //     db.Message.bulkCreate([
+    //         {sender_username_id: 1, receiver_username: "username2", message: "message1"},
+    //         {sender_username_id: 1, receiver_username: "username2", message: "message2"},
+    //         {sender_username_id: 1, receiver_username: "username2", message: "message3"}
+    //     ])
+    // }
 }
 
 module.exports = seed;
