67acdb2267a90c554e4ce27ecd690c4d4afed2a2
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2021-01-07 11:43:37 -0500

    Changed ID's in input elements to reflect the html

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let loginForm = $("#login-form");
-    let userName = $("#user-name");
-    let userPass = $("#user-pass");
+    let userName = $("#username");
+    let userPass = $("#password");
 
     $(document).on("submit","form", function(event) {
         event.preventDefault();
